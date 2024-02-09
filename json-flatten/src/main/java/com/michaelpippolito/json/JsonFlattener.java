0c9bf52787992677377464c675347d68bf09f512
michaelpippolito <michael.p.ippolito@gmail.com>
2024-02-09 15:27:23 -0500

    Initial commit with json-flatten module

---
+++ json-flatten/src/main/java/com/michaelpippolito/json/JsonFlattener.java
@@ -0,0 +1,67 @@
+package com.michaelpippolito.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import lombok.RequiredArgsConstructor;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class JsonFlattener {
+    private static final String DEFAULT_SEPARATOR = "_";
+    private static final String DEFAULT_BASE_PATH = "";
+    public static final String EMPTY_ARRAY = "EMPTY_ARRAY";
+
+    public static Map<String, String> flattenJsonString(String json) throws JsonProcessingException {
+        return flattenJsonString(json, DEFAULT_SEPARATOR, DEFAULT_BASE_PATH);
+    }
+
+    public static Map<String, String> flattenJsonString(String json, String separator, String basePath) throws JsonProcessingException {
+        Map<String, String> flattenedJson = new LinkedHashMap<>();
+        JsonNode jsonNode = new ObjectMapper().readTree(json);
+        jsonNode.fieldNames().forEachRemaining(new JsonFieldConsumer(jsonNode, flattenedJson, basePath, separator));
+        return flattenedJson;
+    }
+
+
+    @RequiredArgsConstructor
+    public static class JsonFieldConsumer implements Consumer<String> {
+        private final JsonNode jsonNode;
+        private final Map<String, String> flattenedJson;
+        private final String basePath;
+        private final String separator;
+
+        @Override
+        public void accept(String fieldName) {
+            JsonNode jsonField = jsonNode.get(fieldName);
+            String flattenedKey = "".equals(basePath) ? fieldName : basePath + separator + fieldName;
+            acceptField(flattenedKey, jsonField);
+        }
+
+        private void acceptArrayField(String flattenedKey, ArrayNode arrayField) {
+            if (arrayField.size() == 0) {
+                flattenedJson.put(flattenedKey, EMPTY_ARRAY);
+            } else {
+                for (int index = 0; index < arrayField.size(); index++) {
+                    JsonNode arrayEntry = arrayField.get(index);
+                    String arrayEntryKey = flattenedKey + separator + index;
+                    acceptField(arrayEntryKey, arrayEntry);
+                }
+            }
+        }
+
+        private void acceptField(String key, JsonNode field) {
+            switch (field.getNodeType()) {
+                case ARRAY -> acceptArrayField(key, (ArrayNode) field);
+                case OBJECT, POJO -> field.fieldNames().forEachRemaining(new JsonFieldConsumer(field, flattenedJson, key, separator));
+                case MISSING, NULL -> flattenedJson.put(key, null);
+                case BINARY, STRING -> flattenedJson.put(key, field.textValue());
+                case BOOLEAN -> flattenedJson.put(key, String.valueOf(field.booleanValue()));
+                case NUMBER -> flattenedJson.put(key, field.numberValue().toString());
+            }
+        }
+    }
+}
---
+++ json-flatten/src/test/java/com/michaelpippolit/json/JsonFlattenerTest.java
@@ -0,0 +1,102 @@
+package com.michaelpippolit.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.michaelpippolito.json.JsonFlattener;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+import static com.michaelpippolito.json.JsonFlattener.EMPTY_ARRAY;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonFlattenerTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testFlattenJsonString() throws IOException {
+        String originalJson =
+                new String(Objects.requireNonNull(JsonFlattenerTest.class.getClassLoader().getResourceAsStream("test.json")).readAllBytes());
+
+
+        Map<String, String> flattenedJson = JsonFlattener.flattenJsonString(originalJson);
+
+        assertTrue(flattenedJson.containsKey("string"));
+        assertEquals("string", flattenedJson.get("string"));
+        assertTrue(flattenedJson.containsKey("boolean"));
+        assertEquals("true", flattenedJson.get("boolean"));
+        assertTrue(flattenedJson.containsKey("number"));
+        assertEquals("1", flattenedJson.get("number"));
+        assertTrue(flattenedJson.containsKey("null"));
+        assertNull(flattenedJson.get("null"));
+        assertTrue(flattenedJson.containsKey("emptyArray"));
+        assertEquals(EMPTY_ARRAY, flattenedJson.get("emptyArray"));
+        assertTrue(flattenedJson.containsKey("primitiveArray_0"));
+        assertEquals("string", flattenedJson.get("primitiveArray_0"));
+        assertTrue(flattenedJson.containsKey("primitiveArray_1"));
+        assertEquals("true", flattenedJson.get("primitiveArray_1"));
+        assertTrue(flattenedJson.containsKey("primitiveArray_2"));
+        assertEquals("1", flattenedJson.get("primitiveArray_2"));
+        assertTrue(flattenedJson.containsKey("primitiveArray_3"));
+        assertNull(flattenedJson.get("primitiveArray_3"));
+
+        assertTrue(flattenedJson.containsKey("object_string"));
+        assertEquals("string", flattenedJson.get("object_string"));
+        assertTrue(flattenedJson.containsKey("object_boolean"));
+        assertEquals("true", flattenedJson.get("object_boolean"));
+        assertTrue(flattenedJson.containsKey("object_number"));
+        assertEquals("1", flattenedJson.get("object_number"));
+        assertTrue(flattenedJson.containsKey("object_null"));
+        assertNull(flattenedJson.get("object_null"));
+        assertTrue(flattenedJson.containsKey("object_emptyArray"));
+        assertEquals(EMPTY_ARRAY, flattenedJson.get("object_emptyArray"));
+        assertTrue(flattenedJson.containsKey("object_primitiveArray_0"));
+        assertEquals("string", flattenedJson.get("object_primitiveArray_0"));
+        assertTrue(flattenedJson.containsKey("object_primitiveArray_1"));
+        assertEquals("true", flattenedJson.get("object_primitiveArray_1"));
+        assertTrue(flattenedJson.containsKey("object_primitiveArray_2"));
+        assertEquals("1", flattenedJson.get("object_primitiveArray_2"));
+        assertTrue(flattenedJson.containsKey("object_primitiveArray_3"));
+        assertNull(flattenedJson.get("object_primitiveArray_3"));
+
+        assertTrue(flattenedJson.containsKey("object_object_string"));
+        assertEquals("string", flattenedJson.get("object_object_string"));
+        assertTrue(flattenedJson.containsKey("object_object_boolean"));
+        assertEquals("true", flattenedJson.get("object_object_boolean"));
+        assertTrue(flattenedJson.containsKey("object_object_number"));
+        assertEquals("1", flattenedJson.get("object_object_number"));
+        assertTrue(flattenedJson.containsKey("object_object_null"));
+        assertNull(flattenedJson.get("object_object_null"));
+        assertTrue(flattenedJson.containsKey("object_object_emptyArray"));
+        assertEquals(EMPTY_ARRAY, flattenedJson.get("object_object_emptyArray"));
+        assertTrue(flattenedJson.containsKey("object_object_primitiveArray_0"));
+        assertEquals("string", flattenedJson.get("object_object_primitiveArray_0"));
+        assertTrue(flattenedJson.containsKey("object_object_primitiveArray_1"));
+        assertEquals("true", flattenedJson.get("object_object_primitiveArray_1"));
+        assertTrue(flattenedJson.containsKey("object_object_primitiveArray_2"));
+        assertEquals("1", flattenedJson.get("object_object_primitiveArray_2"));
+        assertTrue(flattenedJson.containsKey("object_object_primitiveArray_3"));
+        assertNull(flattenedJson.get("object_object_primitiveArray_3"));
+
+        assertTrue(flattenedJson.containsKey("objectArray_0_string"));
+        assertEquals("string", flattenedJson.get("objectArray_0_string"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_boolean"));
+        assertEquals("true", flattenedJson.get("objectArray_0_boolean"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_number"));
+        assertEquals("1", flattenedJson.get("objectArray_0_number"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_null"));
+        assertNull(flattenedJson.get("objectArray_0_null"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_emptyArray"));
+        assertEquals(EMPTY_ARRAY, flattenedJson.get("objectArray_0_emptyArray"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_primitiveArray_0"));
+        assertEquals("string", flattenedJson.get("objectArray_0_primitiveArray_0"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_primitiveArray_1"));
+        assertEquals("true", flattenedJson.get("objectArray_0_primitiveArray_1"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_primitiveArray_2"));
+        assertEquals("1", flattenedJson.get("objectArray_0_primitiveArray_2"));
+        assertTrue(flattenedJson.containsKey("objectArray_0_primitiveArray_3"));
+        assertNull(flattenedJson.get("objectArray_0_primitiveArray_3"));
+    }
+}
